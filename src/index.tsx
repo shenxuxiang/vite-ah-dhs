fbae9f0b74f59f8b18eec3ed67a9c06c6ccc1101
shenxuxiang <shen_xuxiang@163.com>
2023-05-08 16:59:05 +0800

    style: 样式格式化

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import { HashRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 import Router from "@/routers";
-
-
-
 import "./index.less";
 // import '@/mock/mock';
+
+
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
